f0f1930f4cccb1186bb63f8c1c4151ae2171a5e5
kapoorp99 <kapoorprakhar99@gmail.com>
2021-03-04 06:07:39 +0530

    Initial commit

---
+++ api/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True,
+                                     style={'input_type': 'password'})
+
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password')
+        write_only_fields = 'password'
+        read_only_fields = ('is_staff', 'is_superuser', 'is_active',)
+
+    def create(self, validated_data):
+        user = super(CreateUserSerializer, self).create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
---
+++ api/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import get_user_model
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.views import APIView
+from api.serializers import CreateUserSerializer
+from rest_framework import parsers, renderers
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.compat import coreapi, coreschema
+from rest_framework.schemas import ManualSchema
+
+
+class CreateUserAPIView(CreateAPIView):
+    serializer_class = CreateUserSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        # We create a token that will be used for future auth
+        token = Token.objects.create(user=serializer.instance)
+        token_data = {"token": token.key}
+        message = {"message": "User registered successfully",
+                   "status_code": "200",
+                   }
+        return Response(
+            {**message, **serializer.data, **token_data},
+            status=status.HTTP_201_CREATED,
+            headers=headers,
+        )
+
+
+class LogoutUserAPIView(APIView):
+    queryset = get_user_model().objects.all()
+
+    def get(self, request, format=None):
+        # simply delete the token to force a login
+        request.user.auth_token.delete()
+        message = {"statusCode": "200",
+                   "message": "User logged out successfully"}
+        return Response(data=message, status=status.HTTP_200_OK)
+
+
+class ObtainAuthToken(APIView):
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+    if coreapi is not None and coreschema is not None:
+        schema = ManualSchema(
+            fields=[
+                coreapi.Field(
+                    name="username",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Username",
+                        description="Valid username for authentication",
+                    ),
+                ),
+                coreapi.Field(
+                    name="password",
+                    required=True,
+                    location='form',
+                    schema=coreschema.String(
+                        title="Password",
+                        description="Valid password for authentication",
+                    ),
+                ),
+            ],
+            encoding="application/json",
+        )
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        content = {
+                    'token': token.key,
+                    'message': "User logged in successfully",
+                    'status': "200"
+        }
+        return Response(content)
+
+
+obtain_auth_token = ObtainAuthToken.as_view()
